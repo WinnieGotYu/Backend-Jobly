ac8e405abaca253d036c9ea5a040de07dfbe3614
Winnie Yu <winnie@winnie.local>
2020-08-04 17:21:55 -0700

    Initial

---
+++ __tests__/unit/partialUpdate.test.js
@@ -0,0 +1,16 @@
+const sqlForPartialUpdate = require("../../helpers/partialUpdate");
+
+describe("partialUpdate()", () => {
+  it("should generate a proper partial update query with just 1 field",
+      () => {
+    const {query, values} = sqlForPartialUpdate(
+      "users",
+      {first_name: "Test"},
+      "username",
+      "testuser"
+  );
+    expect(query).toEqual("UPDATE users SET first_name=$1 WHERE username=$2 RETURNING *");
+
+    expect(values).toEqual(["Test","testuser"]);
+  });
+});
